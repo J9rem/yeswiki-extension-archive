d4443aad9a45e096d4ed6a3bb33bbfd57198869b
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-09-15 11:42:35 +0200

    feat(UpdateAction): add link to GererSauvegardes page

---
+++ handlers/UpdateHandler__.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * This file is part of the YesWiki Extension archive.
+ *
+ * Authors : see README.md file that was distributed with this source code.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace YesWiki\Archive;
+
+use Exception;
+
+use YesWiki\Core\Service\LinkTracker;
+use YesWiki\Core\Service\PageManager;
+use YesWiki\Core\YesWikiHandler;
+use YesWiki\Security\Controller\SecurityController;
+
+class UpdateHandler__ extends YesWikiHandler
+{
+    protected $pageManager;
+    protected $securityController;
+
+    public function run()
+    {
+        $this->pageManager = $this->getService(PageManager::class);
+        $this->securityController = $this->getService(SecurityController::class);
+
+        if ($this->securityController->isWikiHibernated()) {
+            throw new Exception(_t('WIKI_IN_HIBERNATION'));
+        };
+        if (!$this->wiki->UserIsAdmin()) {
+            return null;
+        }
+
+        $output = 'ℹ️ Adding GererSauvegardes pages.... ';
+        $page = $this->pageManager->getOne('GererSauvegardes');
+        if (empty($page)) {
+            list($updatePagesState, $message) = $this->addGererSauvegardesPage();
+            if ($updatePagesState) {
+                $output .= '✅ Done !<br />';
+            } else {
+                $output .= '<span class="label label-warning">! '._t('UPDATE_ADMIN_PAGES_ERROR').'</span>'.'<br />'.$message;
+            }
+        } else {
+            $output .= '✅ Done !<br />';
+        }
+
+        // set output
+        $this->output = str_replace(
+            '<!-- end handler /update -->',
+            $output.'<!-- end handler /update -->',
+            $this->output
+        );
+        return null;
+    }
+
+    /**
+     * method to update GererSauvegardes page
+     * @return array [bool true/false, string|null errorMessage]
+     */
+    private function addGererSauvegardesPage(): array
+    {
+        $pageContent =
+        <<<CONTENT
+        {{nav class="nav nav-tabs" links="GererMisesAJour, GererSauvegardes" titles="Mises à jour / extensions, Sauvegardes" }}
+        
+        {{adminbackups}}
+        CONTENT;
+        $output = '';
+        $linkTracker = $this->getService(LinkTracker::class);
+        if ($this->getService(PageManager::class)->save('GererSauvegardes', $pageContent) !== 0) {
+            $output .= (!empty($output) ? ', ' : '')._t('NO_RIGHT_TO_WRITE_IN_THIS_PAGE').'GererSauvegardes';
+        } else {
+            // save links
+            $linkTracker->registerLinks($this->getService(PageManager::class)->getOne('GererSauvegardes'));
+        }
+        return [empty($output),$output];
+    }
+}
--- lang/archive_fr.inc.php
+++ lang/archive_fr.inc.php
@@ -14,7 +14,7 @@ return [
     // Commons
     'ARCHIVES' => 'Sauvegardes',
     'ONLY_FOR_ADMINS' => 'Seulement pour les administrateurs',
-    
+
     // actions/AdminBackupsAtion.php
     'ADMIN_BACKUPS_TITLE' => 'Gestion des sauvegardes',
     'ADMIN_BACKUPS_ARCHIVES_LIST' => 'Liste des sauvegardes',
@@ -35,5 +35,7 @@ return [
 
     'EDIT_CONFIG_GROUP_ARCHIVE' => 'Archivage',
     'EDIT_CONFIG_HINT_ARCHIVE[PRIVATEPATH]' => 'Localisation des sauvegardes (vide = dossier temporaire si possible)',
-    
+
+    // templates/autoupdate/core.twig
+    'ARCHIVE_MANAGE_BACKUPS' => 'Gérer les sauvegardes',
 ];
